52796e7e32911532fb6d86f8ecaf47f081d4118c
foragerDev <mohsan0073@gmail.com>
2020-08-09 04:17:09 +0500

    Add insert function.

--- tree.cpp
+++ tree.cpp
@@ -2,9 +2,9 @@
 #include <memory>
 
 struct Node{
-    int* parent; //Parent of the tree
-    int* right; // left child of the tree
-    int* left;  // right child of the tree
+    Node* parent; //Parent of the tree
+    Node* right; // left child of the tree
+    Node* left;  // right child of the tree
     int data;     // data containing in the node
     Node(const int item):
         parent(nullptr),
@@ -20,20 +20,60 @@ struct Node{
 class Tree{
 private:
     std::size_t size;
-    int* root;
+    Node* root;
     
     //return new_node
     Node* new_node(const int item){
         return new Node(item);
- }
+    }
+    void freememory(){
+
+    }
 
 public:
     Tree(): size(0),
     root(nullptr){
 
     }
+
+    void insert(int number){
+        Node* element = new_node(number);
+        Node* parent = nullptr;
+        Node* x = root;
+        while( x != nullptr){
+            parent = x;
+            if(element->data < x->data){
+                x = x->left;
+            }
+            else{
+                x = x->right;
+            }
+        }
+        element->parent = parent;
+        if (parent == nullptr){
+            root = element;
+            ++size;
+        }
+        else{
+            if (element->data < parent->data){
+                parent->left = element;
+                ++size;
+            }
+            else{
+                parent->right = element;
+                ++size;
+            }
+        }
+    }
     
     ~Tree(){
 
     }
 };
+
+int main(){
+    Tree tree;
+    tree.insert(6); //this will result in memory leak, so I have to solve this issue; in the free memory function
+    return 0;
+
+}
